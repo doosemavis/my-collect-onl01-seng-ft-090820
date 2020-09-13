c36ece33c91c330044249206c8f6f70ff4e4379d
Moose Davis <moosedavis2011@gmail.com>
2020-09-13 21:49:30 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,9 @@
 list_of_students = ["Time Jones", "Tom Smith", "Jim Campagno"]
 
 def my_collect(list_of_students)
+  i = 0 
+  new_collection = []
+  while i < collection.length
   my_collect(students) do |student|
     student.split(" ").first 
   end 
